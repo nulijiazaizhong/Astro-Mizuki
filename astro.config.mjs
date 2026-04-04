abcda2d97b1d32bebc19175d44d9ab8dd9aaf987
晚安 <nulijiazaizhong@outlook.com>
2026-04-05 07:39:59 +0800

    补充缺失的内容2

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
 
 	integrations: [
 		umami({
-			shareUrl: false,
+			shareUrl: "https://static.goodnightan.com/share/SjfaaxZ0gZI4ewAL",
 		}),
 		swup({
 			theme: false,
