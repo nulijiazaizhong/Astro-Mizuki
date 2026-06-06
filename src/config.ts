f503f626d8ae2eb9b1cd56fd39ee00d91b6bd881
晚安 <nulijiazaizhong@outlook.com>
2026-06-06 20:50:08 +0800

    修改英文字体为：MapleMono-NF-CN-Light，中文字体为：LikeJianJianTi-Regular

--- src/config.ts
+++ src/config.ts
@@ -213,16 +213,16 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "MapleMono-NF-CN-Light”,LikeJianJianTi-Regular",
+			fontFamily: "MapleMono-NF-CN-Light",
 			fontWeight: "400",
 			localFonts: ["MapleMono-NF-CN-Light.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "ZenMaruGothic-Medium",
+			fontFamily: "LikeJianJianTi-Regular",
 			fontWeight: "500",
-			localFonts: ["ZenMaruGothic-Medium.ttf"],
+			localFonts: ["LikeJianJianTi-Regular.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
